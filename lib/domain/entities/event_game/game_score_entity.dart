65e2f1cbcda66295bdbe056427e332dca8cd8d2b
Rodrigolmg <rodrigo.leitemg@gmail.com>
2023-09-10 12:00:48 -0300

    Removing HiveField

--- lib/domain/entities/event_game/game_score_entity.dart
+++ lib/domain/entities/event_game/game_score_entity.dart
@@ -1,15 +1,10 @@
 part of entity;
 
 @HiveType(typeId: 17)
-class GameScoreEntity extends HiveObject {
+class GameScoreEntity {
 
-  @HiveField(0)
   final int? display;
-
-  @HiveField(1)
   final int? period1;
-
-  @HiveField(2)
   final int? period2;
 
   GameScoreEntity({
